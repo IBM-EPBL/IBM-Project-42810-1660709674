d474dc6771f54fe98a2160540c63be0c9045ecba
Tamizharasan C <98448422+KrishTamil@users.noreply.github.com>
2022-09-28 09:28:03 +0530

    Add files via upload

---
+++ Assignment/Tamizharasan C/Assignment 2/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+     return 'home page'
+@app.route('/about')
+def home():
+     return 'home page'
+@app.route('/signup')
+def signup():
+     return 'signup'
+@app.route('/signin')
+def signup():
+     return 'signin page'
+if __name__ == '__main__':
+     app.run(debug=True)
